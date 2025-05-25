abf6ecf85eefc3ebf32386b0fe364d23131eca1f
Marcos Vinícius Cavalcante <mvinic2011@gmail.com>
2025-05-25 13:26:48 -0300

    feat: connect code with ui

--- CalculatorUIKit/StarterProject/CalculatorViewController.swift
+++ CalculatorUIKit/StarterProject/CalculatorViewController.swift
@@ -1,29 +1,56 @@
-//
-//  CalculatorViewController.swift
-//  CalculatorUIKit
-//
-//  Created by R K on 11/29/23.
-//
-
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
+    @IBOutlet var roundButtons: [UIButton]!
+    
+    @IBOutlet weak var divideButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    lazy var operationButtons: [UIButton] = [
+        divideButton,
+        multiplyButton,
+        minusButton,
+        plusButton
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupButtons()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupButtons() {
+        for button in operationButtons {
+            button.layer.cornerRadius =
+                button.frame.height / 2
+        }
+        
+        for button in roundButtons {
+            button.layer.cornerRadius =
+                button.frame.height / 2
+        }
     }
-    */
 
+    @IBAction func didTapOperationButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapNumberButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapDecimalButton() {
+    }
+    
+    @IBAction func didTapEqualsButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapPercentButton() {
+    }
+    
+    @IBAction func didTapPlusMinusButton() {
+    }
+    
+    @IBAction func didTapClearButton() {
+    }
+    
 }
